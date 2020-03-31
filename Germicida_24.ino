5e024e647c0e7add6df97016b4dc3bce45ba81cc
Santiago Romero Ayala <santiromeroayala@hotmail.com>
2020-03-31 15:09:59 -0300

    updateal germicida27
    -

--- Germicida_24.ino
+++ Germicida_24.ino
@@ -5,7 +5,7 @@
 
  PIN 4-> SALIDA RELEE
  Pin 5 -> SALIDA LED
- Pin 9 -> SALIDA LED_RADIACION
+ Pin 12 -> SALIDA LED_RADIACION
 
  Pin 6 -> SALIDA BUZZER
 
@@ -15,56 +15,37 @@
 */
 #define MOV1 2
 #define MOV2 3
-#define RELEE 4
+//#define RELEE 4
 #define LED 5
-#define LED_RAD 9
+#define LED_RAD 12
 #define BUZZER 6
 #define BTINIT 10
 
 #define TIEMPOOFF 10000  //al detectar a alguien
-#define TIEMPO_PROC 30000 //tiempo total 30min -> 1800000
+#define TIEMPO_PROC 300000 //tiempo total 30min -> 1800000
 
-#define TIEMPO_FINAL 15000 //tiempo total 30min
+#define TIEMPO_FINAL 10000 //tiempo total 30min
+unsigned int TIEMPOOFF_init = 15000; //al iniciar
 
-unsigned int TIEMPOOFF_init = 21000; //al iniciar
 // variables will change:
-volatile int detec_mov1 = 0;         // variable for reading the sensor
-volatile int detec_mov2 = 0;         // variable for reading the sensor
-boolean parar = false;
-boolean btninit = false;
-boolean proceso = false;
-boolean comienza = false;
-boolean finalizo = false;
-
-unsigned long tiempo;
-unsigned long tiempo_2;
-unsigned long tiempo_3;
-unsigned long tiempo_4; //Comienza el ciclo 
-
-unsigned long tiempo_final; //Comienza el ciclo 
-
-unsigned long tiempo_proceso; //Comienza el ciclo 
-
-int btncount = 0;
-
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(LED, OUTPUT);
   pinMode(LED_RAD, OUTPUT);
-
   pinMode(BUZZER, OUTPUT);
-  pinMode(RELEE, OUTPUT);
+  //pinMode(RELEE, OUTPUT);
   pinMode(BUZZER, OUTPUT);
  
   pinMode(BTINIT, INPUT_PULLUP);
   
   pinMode(MOV1, INPUT);
-  pinMode(MOV2, INPUT);
+  //pinMode(MOV2, INPUT);
   
   attachInterrupt(digitalPinToInterrupt(2), pin_ISR, RISING); 
-  attachInterrupt(digitalPinToInterrupt(3), pin_ISR, RISING); 
+  //attachInterrupt(digitalPinToInterrupt(3), pin_ISR, RISING); 
   
   btncount=0;
+  tiempo_perdido = 0;
 }
 
 // the loop function runs over and over again forever
@@ -76,11 +57,10 @@ void loop() {
   {
        delay(300); 
        btncount++;
-   }
+  }
 
   if (btncount > 4)
   {
-    Serial.println("boton apretado");
     tiempo_4 = millis();
     btncount=0;
     comienza = true;
@@ -113,46 +93,50 @@ void loop() {
         
         if (!parar)
             {
-            Serial.println("Esterilizando");  
+           // Serial.println("Esterilizando");  
             analogWrite(BUZZER, 0);        
             digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
             digitalWrite(LED_RAD, HIGH);   // turn the LED on (HIGH is the voltage level)
-            digitalWrite(RELEE, HIGH);   // turn the LED on (HIGH is the voltage level
+            //digitalWrite(RELEE, HIGH);   // turn the LED on (HIGH is the voltage level
+            Serial.write('6');
             }     
         else
             {
-            Serial.println("Paro de esterilizar");
+            //Serial.println("Paro de esterilizar");
             beep_1();
             digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
-            digitalWrite(RELEE, LOW);    // turn the LED off by making the voltage LOW
+           //digitalWrite(RELEE, LOW);    // turn the LED off by making the voltage LOW
+            Serial.write('3');
             digitalWrite(LED_RAD, LOW);
              
                 if((tiempo - tiempo_2) > TIEMPOOFF)
                 {
+                  tiempo_perdido += TIEMPOOFF;
                   parar = false;
                   analogWrite(BUZZER, 0);
                 }
             }
-         if((tiempo - tiempo_proceso) > TIEMPO_PROC)
+         if((tiempo - tiempo_proceso) > (TIEMPO_PROC + tiempo_perdido))
          {
-          Serial.println("FINAL");
+//          Serial.print("FINAL:");
+//          Serial.println(tiempo - tiempo_proceso);
+//          Serial.print(TIEMPO_PROC + tiempo_perdido);
           analogWrite(BUZZER, 0);
-            
           digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
-          digitalWrite(RELEE, LOW);    // turn the LED off by making the voltage LOW
+          //digitalWrite(RELEE, LOW);    // turn the LED off by making the voltage LOW
+          Serial.write('3');
           digitalWrite(LED_RAD, LOW);
           comienza = false;
           proceso = false;
+          tiempo_perdido = 0;
           tiempo_final = millis();
           finalizo = true;
          }
-        
   }
 }
 
 void iniciar() 
 {
-  
  if((tiempo - tiempo_4) < floor(TIEMPOOFF_init/3))
  { 
   beep();
