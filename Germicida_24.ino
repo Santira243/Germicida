74abb54e3789add6fd36c7076f11b4e2cc6fa03c
Santiago Romero Ayala <santiromeroayala@hotmail.com>
2020-04-05 10:55:25 -0300

    Instalado en Paroissiens
    Hospital Paroissien

--- Germicida_24.ino
+++ Germicida_24.ino
@@ -1,15 +1,12 @@
 /*
-
+ Mejoras a incorporar:
+ - Que reporte el tiempo que lleva encendido.
  PIN 2-> Entrada 1 Sensor movimiento
  PIN 3 -> Entrada 2 Sensor movimiento
-
- PIN 4-> SALIDA RELEE
+ PIN 4 -> SALIDA RELEE
  Pin 5 -> SALIDA LED
  Pin 12 -> SALIDA LED_RADIACION
-
  Pin 6 -> SALIDA BUZZER
-
- 
  PIN 10 -> Entrada Boton Inicio CICLO
  
 */
@@ -23,18 +20,18 @@
 
 #define TIEMPOOFF 15000  //al detectar a alguien
 #define TIEMPO_PROC 600000 //tiempo total 20min -> 1200000
-
+#define TIEMPO_HISTERESIS 5000 // tiempo que inhabilita el sensado
 #define TIEMPO_FINAL 10000 //tiempo total 30min
-unsigned int TIEMPOOFF_init = 15000; //al iniciar
+unsigned int TIEMPOOFF_init = 30000; //al iniciar
 
-// variables will change:
-volatile int detec_mov1 = 0;         // variable for reading the sensor
-volatile int detec_mov2 = 0;         // variable for reading the sensor
+volatile int detec_mov1 = 0; // variable for reading the sensor
+volatile int detec_mov2 = 0; // variable for reading the sensor
 boolean parar = false;
 boolean btninit = false;
 boolean proceso = false;
 boolean comienza = false;
 boolean finalizo = false;
+boolean lectura = false;
 
 unsigned long tiempo;
 unsigned long tiempo_2;
@@ -42,56 +39,66 @@ unsigned long tiempo_3;
 unsigned long tiempo_4; //Comienza el ciclo 
 
 unsigned long tiempo_final; //Comienza el ciclo 
-
 unsigned long tiempo_proceso; //Comienza el ciclo 
 unsigned long tiempo_perdido; //por culpa de movimientos
+unsigned long tiempo_encendido; //por culpa de movimientos
+
+int lectura_1;
+int lectura_2;
+
+boolean estado=false; //estado de encendido
+boolean estado_interrupcion;//estado para habilitación de interrupciones
 
 int btncount = 0;
 
-void setup() {
+void setup() 
+{
   Serial.begin(115200);
   pinMode(LED, OUTPUT);
   pinMode(LED_RAD, OUTPUT);
   pinMode(BUZZER, OUTPUT);
-  //pinMode(RELEE, OUTPUT);
-  pinMode(BUZZER, OUTPUT);
- 
   pinMode(BTINIT, INPUT_PULLUP);
-  
   pinMode(MOV1, INPUT);
-  //pinMode(MOV2, INPUT);
-  
-  attachInterrupt(digitalPinToInterrupt(2), pin_ISR, RISING); 
-  //attachInterrupt(digitalPinToInterrupt(3), pin_ISR, RISING); 
-  
+  pinMode(MOV2, INPUT);
+  estado_interrupcion=false;
+  Habilitar_interrupciones(); 
   btncount=0;
   tiempo_perdido = 0;
+  digitalWrite(LED_RAD, HIGH); //Avisa que esta con tensión.
 }
 
-// the loop function runs over and over again forever
-void loop() {
+void loop() 
+{
   tiempo = millis();
   btninit = digitalRead(BTINIT);
   
-  if(!btninit)
+   lectura_1 = digitalRead(MOV1);
+   lectura_2 = digitalRead(MOV2);
+  
+  if(lectura)
   {
+    sensar_movimiento();
+  }
+
+  if(!btninit)
+  {  
        delay(300); 
        btncount++;
   }
-
+  
   if (btncount > 4)
   {
     tiempo_4 = millis();
     btncount=0;
     comienza = true;
     tiempo = millis();
+    digitalWrite(LED_RAD, LOW); // Leyó el botón
   }
   
   if ((comienza) && (!proceso))
   { 
     iniciar();
   }
-
 
   if (finalizo)
   { 
@@ -112,23 +119,19 @@ void loop() {
   {
         
         if (!parar)
-            {
-           // Serial.println("Esterilizando");  
+            {           
             analogWrite(BUZZER, 0);        
             digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
             digitalWrite(LED_RAD, HIGH);   // turn the LED on (HIGH is the voltage level)
-            //digitalWrite(RELEE, HIGH);   // turn the LED on (HIGH is the voltage level
-            Serial.write('6');
+            Encender_luz();  
             }     
         else
             {
             //Serial.println("Paro de esterilizar");
             beep_1();
             digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
-           //digitalWrite(RELEE, LOW);    // turn the LED off by making the voltage LOW
-            Serial.write('3');
+            Apagar_luz();
             digitalWrite(LED_RAD, LOW);
-             
             if((tiempo - tiempo_2) > TIEMPOOFF)
               {
                   tiempo_perdido += TIEMPOOFF;
@@ -136,18 +139,17 @@ void loop() {
                   analogWrite(BUZZER, 0);
                }
             }
+         
          if((tiempo - tiempo_proceso) > (TIEMPO_PROC + tiempo_perdido))
          {
-//          Serial.print("FINAL:");
-//          Serial.println(tiempo - tiempo_proceso);
-//          Serial.print(TIEMPO_PROC + tiempo_perdido);
+          
           analogWrite(BUZZER, 0);
           digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
-          //digitalWrite(RELEE, LOW);    // turn the LED off by making the voltage LOW
-          Serial.write('3');
-          digitalWrite(LED_RAD, LOW);
+          Apagar_luz();
+          digitalWrite(LED_RAD, HIGH);
           comienza = false;
           proceso = false;
+    
           tiempo_perdido = 0;
           tiempo_final = millis();
           finalizo = true;
@@ -157,6 +159,8 @@ void loop() {
 
 void iniciar() 
 {
+ Inhabilitar_interrupciones();  
+  
  if((tiempo - tiempo_4) < floor(TIEMPOOFF_init/3))
  { 
   beep();
@@ -248,28 +252,31 @@ if(abs(tiempo - tiempo_3) > 200)
 void beep_3()
 {
  digitalWrite(LED, LOW);   // turn the LED on (HIGH is the voltage level
- if (abs(tiempo - tiempo_3) < 200)
+ if (abs(tiempo - tiempo_3) < 1000)
     {
     analogWrite(BUZZER, 200); 
-    digitalWrite(LED, LOW);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
     }
-if(abs(tiempo - tiempo_3) > 500)
+if(abs(tiempo - tiempo_3) > 1000)
     {
      analogWrite(BUZZER, 0);
-     digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+     digitalWrite(LED, LOW);   // turn the LED on (HIGH is the voltage level)
     }
 
-if(abs(tiempo - tiempo_3) > 700)
+if(abs(tiempo - tiempo_3) > 2000)
     {
     tiempo_3 = millis();
     }
 }
 
-void pin_ISR() 
+void sensar_movimiento() 
 {
-  parar = true;
-  tiempo_2 = millis();
-  tiempo_3 = millis();
+  if(( lectura_2 ) || (lectura_1))
+  {
+    parar = true;
+    tiempo_2 = millis();
+    tiempo_3 = millis();
+  }
 }
 
 void serialEvent() {
@@ -291,6 +298,39 @@ void serialEvent() {
       { 
         iniciar();
       }
+      parar = false;
    }
 
 }
+
+void Encender_luz()
+  {
+    Serial.write('O'); 
+    if(estado == false)
+   {
+    estado = true;
+    tiempo_encendido = millis();
+   }
+  
+   if(((tiempo - tiempo_encendido) > TIEMPO_HISTERESIS))
+    {
+      Habilitar_interrupciones();
+    }
+  }
+
+void Inhabilitar_interrupciones()
+  {
+    lectura = false;   
+  }
+
+ void Habilitar_interrupciones()
+ {
+  lectura = true;
+ }
+
+void  Apagar_luz()
+ {
+    Serial.write('S');
+    estado = false;
+    lectura = false;
+ }            
